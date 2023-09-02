a19177f799970f7151b351fd4979a22d8aec66e7
srini528 <143832663+srini528@users.noreply.github.com>
2023-09-02 16:14:53 -0500

    Update SpringbootBlogRestApiApplication.java

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -8,6 +8,7 @@ public class SpringbootBlogRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+		//sysout
 	}
 
 }
